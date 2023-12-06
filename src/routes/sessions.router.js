1217cfc6019eb754b11f297b0211b136af12e983
matias <matiasagustinhortas@gmail.com>
2023-12-06 19:54:55 -0300

    se agrega ruta

--- src/routes/sessions.router.js
+++ src/routes/sessions.router.js
@@ -44,7 +44,7 @@ router.post("/login", (req, res, next) => {
 
     res.cookie("token", token, { maxAge: 30000, httpOnly: true });
 
-    return res.redirect("/api/views/profile");
+    return res.redirect("/api/sessions/current");
   })(req, res, next);
 });
 
